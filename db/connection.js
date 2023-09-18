cb73a7b16118ee301a3122b935a4e54d685b59c7
DipakPansheriya <dkpanseriya61@gmail.com>
2023-09-18 13:59:40 +0530

    initial commit

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const db =  mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log( "DataBase"));
+const db =  mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log( "mongodb connection"));
 module.exports = db
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const userRoute =  require('./router/userRoute')
+require('./db/connection')
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 9000;
+const cors = require('cors')
 
+app.use(cors({origin: "*"}));
 
-app.post('/api/post', async (req, res) => {
-    const data = await req.body; // Assuming you're sending JSON in the request body
-  console.log(req.body);
-    // Send a response
-    res.status(200).json({ message: 'Data received successfully', data:data });
-  });
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
   
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.use(express.json());
+app.use('/api/v1/user' , userRoute)
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
